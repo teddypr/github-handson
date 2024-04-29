23de466e18082b69c2928f973c1b287a713b380c
teddypr <negatsuki@icloud.com>
2024-04-29 20:24:13 +0900

    git commit -mプロジェクトをセットアップ git log --online

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main extends Throwable {
+
+    private static java.lang.String String;
+
+    public static void main(String[] args) {
+
+        //Mapを作成
+        var momocloMap = new HashMap<Integer, String>();
+
+        //Mapに値を格納
+        momocloMap.put(1, "玉井詩織: ももクロの若大将");
+        momocloMap.put(2, "百田夏菜子: えくぼは恋の落とし穴");
+        momocloMap.put(3, "佐々木彩夏: ちょっぴりセクシーでおちゃめなももクロのアイドル");
+        momocloMap.put(4, "高木れに: ももクロの鋼少女");
+
+        //Mapの値を取り出して表示
+        for (var number : momocloMap.keySet()) {
+            var value = momocloMap.get(number);
+            System.out.println("出席番号：" + number + "   " + "自己紹介：" + value);
+        }
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("出席番号を入力してください:");
+
+        try {
+            Double.parseDouble(java.lang.String.valueOf(!scanner.hasNextInt()));
+            System.out.println(!scanner.hasNext() + "：この文字列は数字です");
+
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("出席番号は数字で入力してください");
+        }
+
+        int menberId = scanner.nextInt();
+        scanner.close();
+
+
+        //メンバーIDに対するメンバー情報を取得
+        String memberName = momocloMap.get(menberId);
+        if (memberName != null) {
+            System.out.println("メンバー名" + memberName);
+        } else {
+            System.out.println("該当するメンバーはいません");
+        }
+
+
+    }
+}
+
